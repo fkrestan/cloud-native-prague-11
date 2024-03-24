7058781be830c46519179388e6be26099ccb7906
fkrestan <33900315+fkrestan@users.noreply.github.com>
2024-03-25 00:30:44 +0100

    Add Nix env

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          tanka
+          jsonnet
+          k3d
+          yq
+        ];
+      };
+    };
+}
